7a31950db470dffac28ddc658cb4dde05e8a53f6
Alexander <alexbary98@gmail.com>
2023-07-02 18:54:02 +0300

    Adding template witch contains model of developers and developers rest controller with 2 get methods: get all developers and get one developer by id

---
+++ src/main/java/com/baryshnikov/SpringSecurityStudy/rest/DeveloperRestControllerV1.java
@@ -0,0 +1,32 @@
+package com.baryshnikov.SpringSecurityStudy.rest;
+
+import com.baryshnikov.SpringSecurityStudy.model.Developer;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DeveloperRestControllerV1 {
+
+    private List<Developer> DEVELOPERS = Stream.of (
+            new Developer(1L, "Toy", "Devov"),
+            new Developer(2L, "Stewie", "Petrov"),
+            new Developer(3L, "Edward", "Newie")
+    ).collect(Collectors.toList());
+
+    @GetMapping
+    public List<Developer> getAll() {
+        return DEVELOPERS;
+    }
+
+    @GetMapping("/{id}")
+    public Developer getById(@PathVariable Long id) {
+        return DEVELOPERS.stream().filter(developer -> developer.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
